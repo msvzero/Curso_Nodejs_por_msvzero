d7b90e813b17f5f7c86594deb8f6c89719f5b8a6
msvzero <msvzero@gmail.com>
2018-05-03 00:52:55 -0400

    Agregado bootstrap 4, actualizacion de index.pug y layout.pug

--- basicExpressWebsite/app.js
+++ basicExpressWebsite/app.js
@@ -14,9 +14,12 @@ app.set('views', path.join(__dirname, 'views'));
 //We also need to set the view engine
 app.set('view engine', 'pug');
 
+
 //Definimos los middleware que vamos a utilizar
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+//Se especifica que el directorio public sera publico o el directorio estatico.
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Configuramos el puerto de nuestro servidor y un mensaje como callback
 app.listen(3000, () => {
