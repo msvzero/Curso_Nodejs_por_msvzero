6d6ceb3cf3840cf40dea46b9794c3164870112b4
msvzero <msvzero@gmail.com>
2018-05-04 21:39:59 -0400

    GET, PUT y DELETE

--- heroRestAPI/app.js
+++ heroRestAPI/app.js
@@ -31,4 +31,56 @@ app.post('/hero', (request, response) => {
     }, (error) => {
         response.status(400).send(error);
     })
-})
+});
+
+app.get('/hero', (request, response)=>{
+    Hero.find().then((heros) => {
+        response.send(heros)
+    }, (error) =>{
+        response.status(400).send(error);
+    })
+
+});
+
+app.get('/hero/:id', (request, response) => {
+    //En este ejemplo se omite la validacion del id
+    let heroId = request.params.id;
+    Hero.findById(heroId).then((hero) => {
+        if(!hero){
+            response.status(400).send();
+        }
+        response.send(hero);
+    }, (error) => {
+        response.status(400).send(error);
+    });
+});
+
+app.put('/hero/:id', (request, response) => {
+    let update = {
+        nombre: request.body.nombre,
+        fuerza: request.body.fuerza,
+        velocidad: request.body.velocidad
+    }
+    //En este ejemplo se omite la verificacion del id
+    let heroId = request.params.id;
+    Hero.findByIdAndUpdate(heroId, update, {new: true}).then((hero) => {
+        if(!hero){
+            return response.status(404).send();
+        }
+        response.send(hero);
+    }, (error) => {
+        response.status(400).send(error);
+    })
+});
+
+app.delete('/hero/:id', (request, response) => {
+    let heroId = request.params.id;
+    Hero.findByIdAndRemove(heroId).then((hero) => {
+        if(!hero){
+            return response.status(404).send();
+        }
+        response.send(hero);
+    },  (error) => {
+        response.status(400).send(error);
+    })
+});
